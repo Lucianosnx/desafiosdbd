72b30f5d81b86dc8c9d9630798e2dd218dfec8d7
lucianosnx <lucianopc@edu.unifor.br>
2025-01-07 16:39:50 -0300

    fix

--- app.py
+++ app.py
@@ -21,34 +21,6 @@ else:
     # Ler o conteúdo do HTML
     html_content = load_html(html_file)
 
-    # Injetar CSS para remover margens e paddings padrão do Streamlit e ajustar o iframe
-    st.markdown("""
-        <style>
-        /* Remove o padding e margin padrão do Streamlit */
-        .block-container {
-            padding: 0;
-            margin: 0;
-            height: 100vh;
-            width: 100vw;
-        }
-        /* Remove o cabeçalho e o rodapé do Streamlit */
-        header, footer {
-            visibility: hidden;
-        }
-        /* Ajusta o iframe para ocupar toda a tela */
-        .full-screen-iframe {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            border: none;
-            margin: 0;
-            padding: 0;
-        }
-        </style>
-        """, unsafe_allow_html=True)
-
     # Embutir o HTML no Streamlit dentro de um div que ocupa toda a tela
     html_code = f"""
     <div class="full-screen-iframe">
@@ -56,9 +28,3 @@ else:
     </div>
     """
 
-    # Determinar uma altura e largura suficientemente grande para acomodar diferentes tamanhos de tela
-    # Como Streamlit requer valores numéricos, usamos um valor alto e confiamos no CSS para ajustar
-    components.html(
-        html_code,
-        scrolling=False  # Desativa a rolagem do iframe
-    )
